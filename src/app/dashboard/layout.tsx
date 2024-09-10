233f12b1af0e5431922c9f6ff0b43a977defcd1e
Jesus Maldonado <jmaldona18@cuc.edu.co>
2024-09-10 16:13:30 -0500

    feat: add layout dashboard, change metadata

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>\
+      Dashboard 
+      <UserButton />
+    </div>
   )
 }
 
